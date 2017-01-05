495d82f7fac68821bc0f40b060b4e793a700bacd
Kent Inverarity <None>
2017-01-05 12:20:43 +1030

    Add test for #141

--- tests/test_add_curve.py
+++ tests/test_add_curve.py
@@ -23,3 +23,15 @@ def test_add_curve_duplicate():
     l.add_curve('B', b2, descr='b2')
     # assert l.keys == ['DEPT', 'B', 'B']
     assert [c.descr for c in l.curves] == ['', 'b1', 'b2']
+
+def test_add_curve_multi_lasfiles():
+    l1 = las.LASFile()
+    l2 = las.LASFile()
+    a = np.array([1, 2, 3, 4])
+    b = np.array([5, 9, 1, 4])
+    c = np.array([2, 2, 7, 6])
+    l1.add_curve('DEPT', a)
+    l2.add_curve('DEPT', a)
+    l1.add_curve('B', b)
+    l2.add_curve('C', c)
+    assert [i.mnemonic for i in l1.curves] == ['DEPT', 'B'] and [i.mnemonic for i in l2.curves] == ['DEPT', 'C']
